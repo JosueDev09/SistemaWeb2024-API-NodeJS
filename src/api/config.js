b571d6af5bd3a7eb3547815337a679769ba77866
JosueFgCumtual <josuecode27@gmail.com>
2024-10-17 17:54:46 -0600

    Update config.js

--- src/api/config.js
+++ src/api/config.js
@@ -1,13 +1,6 @@
 import { config } from "dotenv";
 config();
 
-//CONEXION CON BASE DE DATOS SQL SERVER
-// export const PORT = process.env.PORT || 3000;
-// export const DB_USER = process.env.DB_USER || "sa";
-// export const DB_PASSWORD = process.env.DB_PASSWORD || "M1rf3l";
-// export const DB_SERVER = process.env.DB_SERVER || "192.168.80.5";
-// export const DB_DATABASE = process.env.DB_DATABASE || "dbSAC";
-// export const DB_PORT = process.env.DB_PORT || 1433;
 
 ///CONNECTION WITH MYSQL DATABASE
 
